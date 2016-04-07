7d852130bcfc58c1f19f9318a5328434966eae3f
Jason Murphy <json.murphy@gmail.com>
2016-04-07 08:36:00 -0500

    Initial Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ck.theming
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ck/theming_test.clj
@@ -0,0 +1,7 @@
+(ns ck.theming-test
+  (:require [clojure.test :refer :all]
+            [ck.theming :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
